f48ee06e27b15f5ae29d73797b2835439c372309
Casper Biemans <casperbiemans@MacBook-Pro-van-Casper.local>
2017-05-31 23:09:24 +0200

    Modified PopUpView.swift so it will be counting down.

--- RSRVR/PopUpView.swift
+++ RSRVR/PopUpView.swift
@@ -13,10 +13,37 @@ class PopUpView: UIView {
     // Declare outlets
     @IBOutlet weak var countNumber: UILabel!
     
+    var timer = Timer()
+    var count = 3
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.8, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
+        
+//        countNumber?.text = "2"
+    }
+    
+    
+    func countDown() {
+        if(count > 0) {
+            count -= 1
+            countNumber?.text = String(count)
+        }
+    }
+
+
+    
     @IBAction func handleDismiss(_ sender: Any) {
         VRViewController().dismissPopUp()
         print("Dismiss")
     }
+    
     
     
 
